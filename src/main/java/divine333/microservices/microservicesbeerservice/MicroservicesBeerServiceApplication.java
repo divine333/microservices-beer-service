f2a889a507bce93d6fdbe30d04fbab3cf231be6f
divine777 <tijanailic933@gmail.com>
2020-04-20 17:00:42 +0200

    Initial commit. Closes#1

---
+++ src/main/java/divine333/microservices/microservicesbeerservice/MicroservicesBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package divine333.microservices.microservicesbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/divine333/microservices/microservicesbeerservice/MicroservicesBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package divine333.microservices.microservicesbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
